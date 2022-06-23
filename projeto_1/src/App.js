a415329c95a0af212b11b0723332dadb10bdf214
rafa-speres <65551999+rafa-speres@users.noreply.github.com>
2022-06-22 23:32:35 -0300

    Add files via upload

--- projeto_1/src/App.js
+++ projeto_1/src/App.js
@@ -1,6 +1,9 @@
 
 import './App.css';
 import Create from './components/create';
+import Read from './components/read';
+import Update from './components/update';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
